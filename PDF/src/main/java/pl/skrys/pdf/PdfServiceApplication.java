e15fe409d18c6ca22b700fb74a53aa65990acd5d
sebakrys <sebastianpkrys@gmail.com>
2022-04-13 12:38:57 +0200

    Pdf service as eureka client

--- PDF/src/main/java/pl/skrys/pdf/PdfServiceApplication.java
+++ PDF/src/main/java/pl/skrys/pdf/PdfServiceApplication.java
@@ -2,8 +2,11 @@ package pl.skrys.pdf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 //a
 @SpringBootApplication
+@EnableEurekaClient
 public class PdfServiceApplication {
 
 	public static void main(String[] args) {

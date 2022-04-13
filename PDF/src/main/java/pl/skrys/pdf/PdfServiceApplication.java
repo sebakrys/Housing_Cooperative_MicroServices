24eba760e15963fbf7d79bb8cbd594390e9c3265
sebakrys <sebastianpkrys@gmail.com>
2022-04-13 11:43:54 +0200

    Eureca + pdf service added without eureca client

---
+++ PDF/src/main/java/pl/skrys/pdf/PdfServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.skrys.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//a
+@SpringBootApplication
+public class PdfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfServiceApplication.class, args);
+	}
+
+}
